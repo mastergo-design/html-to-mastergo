2efea0b2f267cddca603d60e0e838ecc7d98d51f
fake-car <929092762@qq.com>
2022-09-29 15:38:29 +0800

    feat: 增加插件示例

---
+++ src/example/html-mg/ui/App.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <button @click="getNewString">Hello, {{ mg }}</button>
+</template>
+
+<script setup>
+import { ref, toRaw } from 'vue'
+
+const mg = ref('MasterGo')
+
+function getNewString() {
+  mg.value = mg.value === 'MasterGo' ? 'World' : 'MasterGo'
+  parent.postMessage(toRaw(mg.value), '*')
+}
+</script>
