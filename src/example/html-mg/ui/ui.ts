6703ebe5324f4673ad3765d8dbb699a9c9d545c7
fake-car <929092762@qq.com>
2022-10-25 17:30:52 +0800

    feat: 删除冗余代码

--- src/example/html-mg/ui/ui.ts
+++ src/example/html-mg/ui/ui.ts
@@ -7,18 +7,3 @@ import App from './App.vue';
 
 const app = createApp(App);
 app.use(Antd).mount('#app')
-
-type Independent = 'x'
-type NotA<T> = T extends Independent? never: T
-type NotB<T> = Independent extends T ? never : T
-
-type NotX<T> = NotA<T> & NotB<T>
-
-type a = {
-  [key in Exclude<string, Independent>]: string;
-} | { x: number }
-
-let abc: a = {
-  x: '1',
-  y: '',
-}
