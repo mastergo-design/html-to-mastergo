b5bcd9643d0882e666e0e40d400d65484f87b609
fake-car <929092762@qq.com>
2022-10-27 11:09:05 +0800

    feat: 整理代码

--- src/example/html-mg/ui/ui.ts
+++ src/example/html-mg/ui/ui.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import './global.scss'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css';
-
 import App from './App.vue';
 
 const app = createApp(App);
