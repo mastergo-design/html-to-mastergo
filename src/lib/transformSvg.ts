c8f7929cf5d794ead075846904f6ba21861e20d6
fake-car <929092762@qq.com>
2023-01-11 13:28:47 +0800

    feat: 处理svg行内样式的fill和stroke

--- src/lib/transformSvg.ts
+++ src/lib/transformSvg.ts
@@ -7,16 +7,23 @@ export const transformSvg = (node: Element, styles: TargetProps, parentStyles: T
   // 填充
   const fill = styles.fill;
   const stroke = styles.stroke;
+  const color = styles.color;
 
-  // (node as HTMLElement).style.fill = styles.fill;
-  // (node as HTMLElement).style.stroke = styles.stroke;
+  if ((node as HTMLElement).style.fill.toLowerCase() === 'currentcolor') {
+    // fill优先级高
+    (node as HTMLElement).style.fill = /rgba|rgb/.test(fill)? fill : color;
+  }
+  if ((node as HTMLElement).style.stroke.toLowerCase() === 'currentcolor') {
+    // stroke优先级高
+    (node as HTMLElement).style.stroke = /rgba|rgb/.test(stroke)? stroke : color;
+  }
 
   result.type = 'PEN';
   let svgString = node.outerHTML
   // 替换填充
-  svgString = svgString.replace(/(fill="currentColor")/, `fill="${fill !== 'none'? fill : '#3D3D3D'}"`);
+  svgString = svgString.replace(/(fill="currentColor"|fill="")/i, `fill="${/rgba|rgb/.test(fill)? fill : color}"`);
   //替换描边
-  svgString = svgString.replace(/(stroke="currentColor")/, `stroke="${stroke !== 'none'? stroke : '#3D3D3D'}"`);
+  svgString = svgString.replace(/(stroke="currentColor"|stroke="")/i, `stroke="${/rgba|rgb/.test(stroke)? stroke : color}"`);
   // 获取svg string
   result.content = svgString
   Object.assign(result, transLayout(styles, parentStyles, 'PEN'));
